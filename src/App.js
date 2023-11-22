771f5f706740d75865f0fad7eb52f4a03fb51f69
majidkhaleqi <majid.khaleqi2014@gmail.com>
2023-11-23 00:04:24 +0330

    2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Board from "./components/Board";
 import Todos from "./components/Todos";
 import ProductsList from "./components/ProductsList";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import "./App.css";
 import "./styles/index.css";
@@ -14,7 +15,12 @@ function App() {
       <Todos />
       <hr /> */}
       {/* <ProductsList /> */}
-      <h1>APP.JS v2</h1>
+      <div class="topnav">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/shop">SHop</NavLink>
+        <NavLink to="/todos">ToDos</NavLink>
+      </div>
+      <h1>APP.JS</h1>
     </div>
   );
 }
